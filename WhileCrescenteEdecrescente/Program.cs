e067367671cb64a07ca82377fa0e73c59d582755
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-12 11:54:06 -0300

    Adicionar arquivos de projeto.

---
+++ WhileCrescenteEdecrescente/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 2 numeros, quando os números forem iguais o programa será fechado ");
+
+            int x, y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if(x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+            }
+
+        }
+    }
+}
